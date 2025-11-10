094f354dab580c1d9cb393232770fbbafc4b2283
Soledium <nescobar3@hotmail.com>
2025-11-10 11:54:01 -0300

    Commit 2
    Se agrega la imagen para los platos, y el contador para el stock

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,14 +1,56 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.html import format_html
 
 from .models import Categoria, Ingrediente, Plato
 
 class PlatoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'precio', 'categoria',) 
+    list_display = ('nombre', 'precio', 'categoria','vista_plato') 
     list_filter = ('categoria',)
     search_fields = ('nombre', 'descripcion',)
     # Usamos filter_horizontal para seleccionar ingredientes
     filter_horizontal = ('ingredientes',) 
 
+    def vista_plato(self, obj):
+        if obj.imagen_plato:
+            return f'<img src="{obj.imagen_plato.url}" width="100" height="100" />'
+        return "No Image"
+
 admin.site.register(Categoria)
+
+
 admin.site.register(Ingrediente)
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'stock')
+    search_fields = ('nombre',)
+    actions = ['agregar_1', 'consumir_1']
+
+    def _adjust(self, request, queryset, delta: int):
+        actualizados = 0
+        omitidos = 0
+        for ing in queryset:
+            nuevo = (ing.stock or 0) + delta
+            if nuevo < 0:
+                omitidos += 1
+                continue
+            ing.stock = nuevo
+            ing.save(update_fields=['stock'])
+            actualizados += 1
+
+        if actualizados:
+            messages.success(request, f"Se actualizaron {actualizados} ingrediente(s).")
+        if omitidos:
+            messages.warning(request, f"{omitidos} ingrediente(s) omitido(s) (no se puede dejar stock negativo).")
+
+    def agregar_1(self, request, queryset):
+        """➕ Agregar 1 unidad"""
+        self._adjust(request, queryset, +1)
+    agregar_1.short_description = "➕ Agregar 1"
+
+    def consumir_1(self, request, queryset):
+        """➖ Consumir 1 unidad"""
+        self._adjust(request, queryset, -1)
+    consumir_1.short_description = "➖ Consumir 1"
+
+
+
 admin.site.register(Plato, PlatoAdmin)
--- mainApp/models.py
+++ mainApp/models.py
@@ -11,15 +11,17 @@ class Categoria(models.Model):
 
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
-    
+    stock = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.nombre
 
 class Plato(models.Model):
-    """Corresponde a la tabla Menu en tu diagrama de clases."""
+    """Corresponde a la tabla Menu en el diagrama de clases."""
     nombre = models.CharField(max_length=150, unique=True)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
+    imagen_plato = models.ImageField(upload_to='platos/', null=True, blank=True)
     
     # Relaciones
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
